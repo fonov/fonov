ddac8b037176030e9563485fb58be35bb8e9fb7e
Fonov Sergei <ask@mounlion.com>
2017-12-20 22:17:51 +0300

    Add scene 7

--- FonovMobile/app/components/test7.js
+++ FonovMobile/app/components/test7.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions, Image } from 'react-native'
 import { Content, Button, Grid, Col } from 'native-base'
+import NextButton from '../elements/nextButton'
 import { Actions } from 'react-native-router-flux'
 
 
@@ -8,9 +9,19 @@ export default class Test7 extends Component{
 
     render(){
 
+        const { width } = Dimensions.get('window');
+
         return(
-            <Content style={{padding: 10}}>
-                <Text>Тест 7</Text>
+            <Content>
+                <Text style={{margin: 10}}>
+                    Откройти центр управления взмахом снизу вверх. Нажмите на клавишу фанарик и проверьте работу светадиода.
+                </Text>
+                <Image
+                    resizeMode='contain'
+                    style={{width: width-20, height: 350, marginLeft: 10, marginTop: 10}}
+                    source={require('../image/test7/flashlight.png')}
+                />
+                <NextButton onPress={() => Actions.Test8()}/>
             </Content>
         )
     }
