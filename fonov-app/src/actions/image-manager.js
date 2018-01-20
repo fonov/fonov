34dd2dd771fe4502f1dd1139ef0e12ebe88bc22b
Fonov Sergei <ask@mounlion.com>
2018-01-20 22:45:38 +0300

    Усовершенствовал

--- fonov-app/src/actions/image-manager.js
+++ fonov-app/src/actions/image-manager.js
@@ -11,22 +11,26 @@ const image_manager = (test, number) => {
         // const current_iphone = {model: 'iPhone 6', color: 'Gold'},
         //     {model, color} = current_iphone;
 
-        if(typeof maps[test] === 'undefined') {
-            throw new Error(`Test "${test}" not found!`)
-        } else if (typeof maps[test]["default"] !== 'undefined') {
-            if (typeof maps[test]["default"][number] === 'undefined') {
-                throw new Error(`Image ${number} for ${test}/default not found!`)
+        if (model && color) {
+            if(typeof maps[test] === 'undefined') {
+                throw new Error(`Test "${test}" not found!`)
+            } else if (typeof maps[test]["default"] !== 'undefined') {
+                if (typeof maps[test]["default"][number] === 'undefined') {
+                    throw new Error(`Image ${number} for ${test}/default not found!`)
+                } else {
+                    return get_absolut_path(maps[test]["default"][number])
+                }
+            } else if (typeof maps[test][model] === 'undefined') {
+                throw new Error(`Model "${model}" for ${test} not found!`)
+            } else if (typeof maps[test][model][color] === 'undefined') {
+                throw new Error(`Color "${color}" for ${test}/${model} not found!`)
+            } else if (typeof maps[test][model][color][number] === 'undefined'){
+                throw new Error(`Image ${number} for ${test}/${model}/${color} not found!`)
             } else {
-                return get_absolut_path(maps[test]["default"][number])
+                return get_absolut_path(maps[test][model][color][number])
             }
-        } else if (typeof maps[test][model] === 'undefined') {
-            throw new Error(`Model "${model}" for ${test} not found!`)
-        } else if (typeof maps[test][model][color] === 'undefined') {
-            throw new Error(`Color "${color}" for ${test}/${model} not found!`)
-        } else if (typeof maps[test][model][color][number] === 'undefined'){
-            throw new Error(`Image ${number} for ${test}/${model}/${color} not found!`)
         } else {
-            return get_absolut_path(maps[test][model][color][number])
+            return get_absolut_path('default.png')
         }
     }
 };
