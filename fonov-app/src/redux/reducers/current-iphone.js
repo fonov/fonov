4b35e8f9e44efa89dd91eb42cd1c607248a65447
Fonov Sergei <ask@mounlion.com>
2018-01-23 20:01:20 +0300

    Исправил тексты и поправил режим разработки

--- fonov-app/src/redux/reducers/current-iphone.js
+++ fonov-app/src/redux/reducers/current-iphone.js
@@ -1,10 +1,5 @@
 let init_state = {model: null, color: null, model_info: {firstLetter: null, code: null, code_country: null}};
 
-if (process.env.NODE_ENV === 'development') {
-    init_state = {model: 'iPhone 5c', color: 'green', model_info: {firstLetter: 'M', code: 'QCT2', code_country: 'RR'}};
-    // init_state = {model: 'iPhone 3gs', color: 'gold', model_info: {firstLetter: 'M', code: 'B717', code_country: 'RR'}};
-}
-
 const current_iphone = (state = init_state, action) => {
     switch (action.type) {
         case 'SET_IPHONE':
@@ -12,7 +7,15 @@ const current_iphone = (state = init_state, action) => {
         case 'CLEAN_IPHONE':
             return Object.assign({}, init_state);
         default:
-            return state
+            if (process.env.NODE_ENV === 'development') {
+                return {
+                    model: 'iPhone 5c',
+                    color: 'green',
+                    model_info: {firstLetter: 'M', code: 'QCT2', code_country: 'RR'}
+                };
+            } else {
+                return state
+            }
     }
 };
 
